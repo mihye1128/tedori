5c1e1a892212b6e268560f197035c5f6190cbab7
mitsui <m.mihye1128@gmail.com>
2020-07-12 11:52:58 +0900

    feat(AngularFire):AngularFire初期設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireFunctionsModule, REGION } from '@angular/fire/functions';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireFunctionsModule,
+  ],
+  providers: [{ provide: REGION, useValue: 'asia-northeast1' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCrOmX1JjarQ29OrT4DpqxFNB7Nl0BlbjY',
+    authDomain: 'tedori-beta.firebaseapp.com',
+    databaseURL: 'https://tedori-beta.firebaseio.com',
+    projectId: 'tedori-beta',
+    storageBucket: 'tedori-beta.appspot.com',
+    messagingSenderId: '368500501250',
+    appId: '1:368500501250:web:0d0d9cdccf205efe61f01b',
+    measurementId: 'G-52R11BTJNV',
+  },
 };
