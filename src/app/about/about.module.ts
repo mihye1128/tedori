8c46046283357b27f12f2a6d22cc6254bc8f2232
mitsui <m.mihye1128@gmail.com>
2020-07-12 14:56:20 +0900

    feat(hero):ヒーロービュー作成

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
+import { SharedModule } from '../shared/shared.module';
 import { AboutComponent } from './about/about.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
-  declarations: [AboutComponent],
-  imports: [CommonModule, AboutRoutingModule],
+  declarations: [AboutComponent, HeroComponent],
+  imports: [CommonModule, AboutRoutingModule, SharedModule],
 })
 export class AboutModule {}
