b1b04d98f8c6f4600802a447b7e531650806cdfc
mitsui <m.mihye1128@gmail.com>
2020-07-12 15:22:09 +0900

    feat(use):サービスLP画面ご利用シーンコンテンツ作成

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -5,9 +5,10 @@ import { AboutRoutingModule } from './about-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { AboutComponent } from './about/about.component';
 import { HeroComponent } from './hero/hero.component';
+import { UseComponent } from './use/use.component';
 
 @NgModule({
-  declarations: [AboutComponent, HeroComponent],
+  declarations: [AboutComponent, HeroComponent, UseComponent],
   imports: [CommonModule, AboutRoutingModule, SharedModule],
 })
 export class AboutModule {}
---
+++ src/app/about/use/use.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UseComponent } from './use.component';
+
+describe('UseComponent', () => {
+  let component: UseComponent;
+  let fixture: ComponentFixture<UseComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [UseComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
