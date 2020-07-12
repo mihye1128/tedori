dab612784835e7e56fada5800c38b0beb5f6ca08
mitsui <m.mihye1128@gmail.com>
2020-07-12 22:12:56 +0900

    feat(interfaces):条件設定のinterface作成

---
+++ src/app/interfaces/condition.ts
@@ -0,0 +1,18 @@
+export interface Condition {
+  title: string;
+  type: 'monthly' | 'hourly';
+  base: number;
+  allowance: number;
+  travelCost: number;
+  basePerHour: number;
+  travelCostPerDay: number;
+  hourPerDay: number;
+  dayPerMonth: number;
+  ins: boolean;
+  unemploymentIns: boolean;
+  area: string;
+  age: 'young' | 'middle' | 'elderly';
+  dependents: number;
+  cityTax: number;
+  otherDeduction: number;
+}
