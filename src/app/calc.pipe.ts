606342f6b1b1bfa839d05803b5d500315cab742b
mitsui <m.mihye1128@gmail.com>
2020-08-04 12:35:54 +0900

    feat(result):雇用保険の計算、Pipeにコメントを挿入

--- src/app/calc.pipe.ts
+++ src/app/calc.pipe.ts
@@ -13,9 +13,12 @@ export class CalcPipe implements PipeTransform {
     let total: number; // 総支給
 
     let compensationIns: number; // 労災保険
+    let unemploymentInsOwner: number; // 雇用保険・事業者
+    let unemploymentInsWorker: number; // 雇用保険・労働者
 
     let target: number;
 
+    // 支給額
     if (condition.type === 'hourly') {
       baseSalary =
         condition.basePerHour * condition.hourPerDay * condition.dayPerMonth;
@@ -26,15 +29,34 @@ export class CalcPipe implements PipeTransform {
       allowance = condition.allowance;
       travelCost = condition.travelCost;
     }
-
     total = baseSalary + allowance + travelCost;
 
+    // 各種保険
     if (rate) {
-      compensationIns = (total * rate.compensationIns.rate) / 1000;
+      // 労災保険
+      compensationIns = Math.floor((total * rate.compensationIns.rate) / 1000);
+
+      // 雇用保険（事業者・労働者）
+      if (condition.unemploymentIns) {
+        unemploymentInsOwner = Math.floor(
+          (total * rate.unemploymentIns.ownerBurden) / 1000
+        );
+        unemploymentInsWorker = Math.floor(
+          (total * rate.unemploymentIns.workerBurden) / 1000
+        );
+      } else {
+        unemploymentInsOwner = 0;
+        unemploymentInsWorker = 0;
+      }
+
+      // 社会保険（健康保険・厚生年金・介護保険・子育て拠出金）
+      if (condition.ins) {
+      }
     } else {
       rate = null;
     }
 
+    // 出力する値
     if (type === 'baseSalary') {
       target = baseSalary;
     } else if (type === 'travelCost') {
@@ -43,6 +65,10 @@ export class CalcPipe implements PipeTransform {
       target = total;
     } else if (type === 'compensationIns') {
       target = compensationIns;
+    } else if (type === 'unemploymentInsOwner') {
+      target = unemploymentInsOwner;
+    } else if (type === 'unemploymentInsWorker') {
+      target = unemploymentInsWorker;
     }
 
     return target.toLocaleString();
