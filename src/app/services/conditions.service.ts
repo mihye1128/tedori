5b0b17b063cf7ec94bbb95223c26e48078f76f6a
mitsui <m.mihye1128@gmail.com>
2020-08-12 00:13:02 +0900

    feat(ConditionsServise):条件保存完了時にSnackbarを表示

--- src/app/services/conditions.service.ts
+++ src/app/services/conditions.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Condition } from '../interfaces/condition';
 import { Subject } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ export class ConditionsService {
   conditions = new Subject<Condition[]>();
   conditions$ = this.conditions.asObservable();
 
-  constructor(private db: AngularFirestore) {}
+  constructor(private db: AngularFirestore, private snackBar: MatSnackBar) {}
 
   setConditions(conditions: Condition[]) {
     this.conditions.next(conditions);
@@ -20,7 +21,14 @@ export class ConditionsService {
     this.conditions.next(conditions);
     for (const condition of conditions) {
       const id = this.db.createId();
-      this.db.doc(`conditions/${id}`).set(condition);
+      this.db
+        .doc(`conditions/${id}`)
+        .set(condition)
+        .then(() => {
+          this.snackBar.open('マイページに保存しました。', '確認する', {
+            duration: 2000,
+          });
+        });
     }
   }
 }
