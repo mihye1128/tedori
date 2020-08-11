80bda58b88fd621d25dcdea1133d25df7a2abda3
mitsui <m.mihye1128@gmail.com>
2020-08-11 22:47:23 +0900

    fix(form):就業地の都道府県リストをRateServiceから取得

--- src/app/home/form/form.component.ts
+++ src/app/home/form/form.component.ts
@@ -9,6 +9,7 @@ import {
 import { Condition } from 'src/app/interfaces/condition';
 import { AuthService } from 'src/app/services/auth.service';
 import { ConditionsService } from 'src/app/services/conditions.service';
+import { RateService } from 'src/app/services/rate.service';
 
 @Component({
   selector: 'app-form',
@@ -19,65 +20,19 @@ export class FormComponent implements OnInit {
   user$ = this.authService.afUser$;
   uid: string;
 
+  rate$ = this.rateService.getRate();
+
   public formGroup: FormGroup;
   public formConditions = ['first', 'second'];
   public formSelect: 'single' | 'malti';
 
   dependents = [...Array(7)].map((_, i) => i + 1);
-  areas = [
-    '北海道',
-    '青森県',
-    '岩手県',
-    '宮城県',
-    '秋田県',
-    '山形県',
-    '福島県',
-    '茨城県',
-    '栃木県',
-    '群馬県',
-    '埼玉県',
-    '千葉県',
-    '東京都',
-    '神奈川県',
-    '新潟県',
-    '富山県',
-    '石川県',
-    '福井県',
-    '山梨県',
-    '長野県',
-    '岐阜県',
-    '静岡県',
-    '愛知県',
-    '三重県',
-    '滋賀県',
-    '京都府',
-    '大阪府',
-    '兵庫県',
-    '奈良県',
-    '和歌山県',
-    '鳥取県',
-    '島根県',
-    '岡山県',
-    '広島県',
-    '山口県',
-    '徳島県',
-    '香川県',
-    '愛媛県',
-    '高知県',
-    '福岡県',
-    '佐賀県',
-    '長崎県',
-    '熊本県',
-    '大分県',
-    '宮崎県',
-    '鹿児島県',
-    '沖縄県',
-  ];
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private conditionsService: ConditionsService
+    private conditionsService: ConditionsService,
+    private rateService: RateService
   ) {
     this.formGroup = this.fb.group({
       formConditions: this.fb.array([]),
