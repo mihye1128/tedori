c132aac1bd30702a9bfedc463519dcfd56c7c9c3
mitsui <m.mihye1128@gmail.com>
2020-08-14 22:41:45 +0900

    fix(mypage):絞り込み条件フォーム仕様変更

--- src/app/mypage/form/form.component.ts
+++ src/app/mypage/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Deductions } from 'src/app/interfaces/deductions';
 import { FormBuilder, Validators } from '@angular/forms';
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-form',
@@ -12,7 +13,7 @@ export class FormComponent implements OnInit {
 
   form = this.fb.group({
     title: ['', [Validators.maxLength(20)]],
-    type: ['title', [Validators.pattern(/title|monthly|hourly/)]],
+    type: ['all', [Validators.pattern(/monthly|hourly|all/)]],
     baseLower: ['', [Validators.maxLength(8)]],
     baseUpper: ['', [Validators.maxLength(8)]],
     allowanceLower: ['', [Validators.maxLength(8)]],
@@ -21,7 +22,7 @@ export class FormComponent implements OnInit {
     basePerHourUpper: ['', [Validators.maxLength(6)]],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private searchService: SearchService) {}
 
   ngOnInit(): void {}
 }
