7c53b52f8392691622738bf82230bc63a6e01272
mitsui <m.mihye1128@gmail.com>
2020-08-16 02:15:20 +0900

    feat(about):機能紹介コンテンツ作成

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from '../shared/shared.module';
 import { AboutComponent } from './about/about.component';
 import { HeroComponent } from './hero/hero.component';
 import { UseComponent } from './use/use.component';
+import { PointsComponent } from './points/points.component';
 
 @NgModule({
-  declarations: [AboutComponent, HeroComponent, UseComponent],
+  declarations: [AboutComponent, HeroComponent, UseComponent, PointsComponent],
   imports: [CommonModule, AboutRoutingModule, SharedModule],
 })
 export class AboutModule {}
