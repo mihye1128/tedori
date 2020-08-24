03edf9db9d6a11a9195f255a07108ff12192217b
mitsui <m.mihye1128@gmail.com>
2020-08-24 22:17:50 +0900

    refactor:formComponent

--- src/app/home/form/form.component.ts
+++ src/app/home/form/form.component.ts
@@ -17,98 +17,64 @@ import { Deductions } from 'src/app/interfaces/deductions';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  user$ = this.authService.afUser$;
-  uid: string;
-
   @Input() rate: Deductions;
 
-  public formGroup: FormGroup;
-  public formConditions = ['first', 'second'];
-  public formSelect: 'single' | 'malti';
+  user$ = this.authService.afUser$;
+  uid: string;
+  formGroup: FormGroup;
+  dependentsCounts = [...Array(7)].map((_, i) => i + 1);
 
-  dependents = [...Array(7)].map((_, i) => i + 1);
+  private conditionsCount = 2;
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
     private conditionsService: ConditionsService
   ) {
-    this.formGroup = this.fb.group({
-      formConditions: this.fb.array([]),
-    });
+    this.buildForm();
+  }
 
-    this.formGroup.addControl(
-      'formSelect',
-      new FormControl('single', [
-        Validators.required,
-        Validators.pattern(/single|multi/),
-      ])
-    );
-
-    this.formConditions.map(() => {
-      const conditionGroup = this.fb.group({});
-
-      conditionGroup.addControl(
-        'title',
-        new FormControl('', [Validators.maxLength(20)])
-      );
-      conditionGroup.addControl(
-        'type',
-        new FormControl('monthly', [Validators.pattern(/monthly|hourly/)])
-      );
-      conditionGroup.addControl(
-        'base',
-        new FormControl('', [Validators.maxLength(8)])
-      );
-      conditionGroup.addControl(
-        'allowance',
-        new FormControl('', [Validators.maxLength(8)])
-      );
-      conditionGroup.addControl(
-        'travelCost',
-        new FormControl('', [Validators.maxLength(7)])
-      );
-      conditionGroup.addControl(
-        'basePerHour',
-        new FormControl('', [Validators.maxLength(6)])
-      );
-      conditionGroup.addControl(
-        'travelCostPerDay',
-        new FormControl('', [Validators.maxLength(5)])
-      );
-      conditionGroup.addControl(
-        'hourPerDay',
-        new FormControl('', [Validators.maxLength(2)])
-      );
-      conditionGroup.addControl(
-        'dayPerMonth',
-        new FormControl('', [Validators.maxLength(2)])
-      );
-      conditionGroup.addControl('ins', new FormControl(true, []));
-      conditionGroup.addControl('unemploymentIns', new FormControl(true, []));
-      conditionGroup.addControl('area', new FormControl('東京都', []));
-      conditionGroup.addControl(
-        'age',
-        new FormControl('young', [Validators.pattern(/young|middle|elderly/)])
-      );
-      conditionGroup.addControl('dependents', new FormControl(0, []));
-      conditionGroup.addControl('cityTax', new FormControl('', []));
-      conditionGroup.addControl(
-        'otherDeduction',
-        new FormControl('', [Validators.maxLength(8)])
-      );
-
-      (this.formGroup.get('formConditions') as FormArray).push(conditionGroup);
-    });
+  get formConditions(): FormArray {
+    return this.formGroup.get('formConditions') as FormArray;
   }
 
   ngOnInit(): void {}
 
-  formDataPush(data: Condition[], condition: Condition) {
+  buildForm() {
+    this.formGroup = this.fb.group({
+      formConditions: this.fb.array([]),
+      formSelect: [
+        'single',
+        [Validators.required, Validators.pattern(/single|multi/)],
+      ],
+    });
+    for (let i = 0; i < this.conditionsCount; i++) {
+      const conditionGroup = this.fb.group({
+        title: ['', [Validators.maxLength(20)]],
+        type: ['monthly', [Validators.pattern(/monthly|hourly/)]],
+        base: ['', [Validators.maxLength(8)]],
+        allowance: ['', [Validators.maxLength(8)]],
+        travelCost: ['', [Validators.maxLength(7)]],
+        basePerHour: ['', [Validators.maxLength(6)]],
+        travelCostPerDay: ['', [Validators.maxLength(5)]],
+        hourPerDay: ['', [Validators.maxLength(2)]],
+        dayPerMonth: ['', [Validators.maxLength(2)]],
+        ins: [true, []],
+        unemploymentIns: [true, []],
+        area: ['東京都', []],
+        age: ['young', [Validators.pattern(/young|middle|elderly/)]],
+        dependents: [0, []],
+        cityTax: ['', []],
+        otherDeduction: ['', [Validators.maxLength(8)]],
+      });
+      this.formConditions.push(conditionGroup);
+    }
+  }
+
+  transferData(condition: Condition): Condition {
     const base = condition.type === 'monthly' ? +condition.base : 0;
     const allowance = condition.type === 'monthly' ? +condition.allowance : 0;
     const travelCost = condition.type === 'monthly' ? +condition.travelCost : 0;
-
     const basePerHour =
       condition.type === 'hourly' ? +condition.basePerHour : 0;
     const travelCostPerDay =
@@ -116,7 +82,7 @@ export class FormComponent implements OnInit {
     const hourPerDay = condition.type === 'hourly' ? +condition.hourPerDay : 0;
     const dayPerMonth =
       condition.type === 'hourly' ? +condition.dayPerMonth : 0;
-    data.push({
+    return {
       title: condition.title,
       type: condition.type,
       base,
@@ -134,20 +100,18 @@ export class FormComponent implements OnInit {
       cityTax: +condition.cityTax,
       otherDeduction: +condition.otherDeduction,
       userId: this.authService.uid,
-    });
+    };
   }
 
   submit() {
     const formValue = this.formGroup.value;
     const conditions = formValue.formConditions;
-    const formData: Condition[] = [];
+    let formData: Condition[];
 
     if (formValue.formSelect === 'single') {
-      this.formDataPush(formData, conditions[0]);
+      formData = [this.transferData(conditions[0])];
     } else {
-      for (const condition of conditions) {
-        this.formDataPush(formData, condition);
-      }
+      formData = conditions.map((condition) => this.transferData(condition));
     }
     this.conditionsService.setConditions(formData);
   }
