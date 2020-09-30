ed38f95e1a21da94574ef66d80fcc1cb41a0bc7d
mitsui <m.mihye1128@gmail.com>
2020-09-30 22:42:54 +0900

    fix(seoService):addTagsからupdateTagへ変更、refactor

--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -5,34 +5,21 @@ import { Meta, Title } from '@angular/platform-browser';
   providedIn: 'root',
 })
 export class SeoService {
-  readonly siteName = 'TEDORI β';
+  readonly siteName = 'TEDORIβ';
   readonly defaultDescription =
     'TEDORIβは、条件を入力するだけで、簡単に差し引き支給額や事業主負担額を試算できます。';
 
   constructor(private titleService: Title, private meta: Meta) {}
 
-  pageTitle(title?: string) {
-    if (title) {
-      return title + ' | ' + this.siteName;
-    } else {
-      return this.siteName + ' | 給与試算ツール';
-    }
-  }
-
-  pageDescription(description?: string) {
-    if (description) {
-      return description;
-    } else {
-      return this.defaultDescription;
-    }
-  }
-
   setTitleAndMeta(title?: string, description?: string) {
-    this.titleService.setTitle(this.pageTitle(title));
-    this.meta.addTags([
+    const pageTitle = title
+      ? title + ' | ' + this.siteName
+      : this.siteName + ' | 給与試算ツール';
+    const pageDescription = description ? description : this.defaultDescription;
+    const metaList = [
       {
         name: 'description',
-        content: this.pageDescription(description),
+        content: pageDescription,
       },
       {
         property: 'og:site_name',
@@ -40,11 +27,11 @@ export class SeoService {
       },
       {
         property: 'og:title',
-        content: this.pageTitle(title),
+        content: pageTitle,
       },
       {
         property: 'og:description',
-        content: this.pageDescription(description),
+        content: pageDescription,
       },
       {
         property: 'og:url',
@@ -54,6 +41,11 @@ export class SeoService {
         property: 'og:image',
         content: location.href + 'assets/images/ogp-cover.png',
       },
-    ]);
+    ];
+
+    this.titleService.setTitle(pageTitle);
+    metaList.forEach((meta) => {
+      this.meta.updateTag(meta);
+    });
   }
 }
