b5fec0bfc7bcce76b3f25e96d66ccfcdf0ba3708
mitsui <m.mihye1128@gmail.com>
2020-10-06 18:59:24 +0900

    fix(about):LP下部にコンテンツ追加

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -8,6 +8,7 @@ import { HeroComponent } from './hero/hero.component';
 import { UseComponent } from './use/use.component';
 import { PointsComponent } from './points/points.component';
 import { HeadingComponent } from './heading/heading.component';
+import { AboutFooterComponent } from './about-footer/about-footer.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { HeadingComponent } from './heading/heading.component';
     UseComponent,
     PointsComponent,
     HeadingComponent,
+    AboutFooterComponent,
   ],
   imports: [CommonModule, AboutRoutingModule, SharedModule],
 })
