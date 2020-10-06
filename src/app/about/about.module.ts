78625ea8173fbd6a302c43efd503b4a3d47b883b
mitsui <m.mihye1128@gmail.com>
2020-10-06 18:37:16 +0900

    fix(about):LP、コピー追加

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -7,9 +7,16 @@ import { AboutComponent } from './about/about.component';
 import { HeroComponent } from './hero/hero.component';
 import { UseComponent } from './use/use.component';
 import { PointsComponent } from './points/points.component';
+import { HeadingComponent } from './heading/heading.component';
 
 @NgModule({
-  declarations: [AboutComponent, HeroComponent, UseComponent, PointsComponent],
+  declarations: [
+    AboutComponent,
+    HeroComponent,
+    UseComponent,
+    PointsComponent,
+    HeadingComponent,
+  ],
   imports: [CommonModule, AboutRoutingModule, SharedModule],
 })
 export class AboutModule {}
---
+++ src/app/about/heading/heading.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heading',
+  templateUrl: './heading.component.html',
+  styleUrls: ['./heading.component.scss'],
+})
+export class HeadingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
