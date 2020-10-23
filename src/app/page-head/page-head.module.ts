fd1c2959e7cca03ddbbeb9285026f5d38b29a1d8
mitsui <m.mihye1128@gmail.com>
2020-10-23 14:12:51 +0900

    fix(page-head):ページタイトルコンポーネントの作成と付随するスタイル修正

---
+++ src/app/page-head/page-head.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageHeadComponent } from './page-head/page-head.component';
+
+@NgModule({
+  declarations: [PageHeadComponent],
+  imports: [CommonModule],
+  exports: [PageHeadComponent],
+})
+export class PageHeadModule {}
